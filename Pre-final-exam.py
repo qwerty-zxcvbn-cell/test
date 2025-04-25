ea4065a674345d0454748c505f0e4d6eee94226e
Christian Sumobay <qwertyzxcvbn808@gmail.com>
2025-04-24 20:18:23 -0700

    Resolved merge conflicts in Pre-final-exam.py

--- Pre-final-exam.py
+++ Pre-final-exam.py
@@ -1,15 +1,9 @@
-#Pre-final Examanition
+#Pre-final Examination
 
 #List of students:
-<<<<<<< HEAD
 1.Labajo, James Ryan N. # dili ni consider kay dli match sa name sa nag commit.
 2.kleinmigz
 3.Sumobay, Christian Paul A.
-=======
-1.
-2.Sumobay, Christian Paul A.
-3.
->>>>>>> ed5bdf467368268b483eacbc9d85bdace4439d1a
 4.
 5.
 6.
